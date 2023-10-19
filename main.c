4ab8dc13e96c9fdd58efed490419568ec152f9be
dageon <93782184+dageon@users.noreply.github.com>
2023-10-19 16:16:14 +0900

    6-1

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
+int all_files;
+
+extern void sub();
+
 int main(void)
 {
-	int i=10;
-	printf("함수 호출전 i=%d\n", i);
-	inc(i);
-	printf("함수 호출후 i=%d\n", i);
+	sub();
+	printf("%d\n", all_files);
 	return 0;
-}
-int inc(int counter)
-{
-	counter++;
-	return counter;
 }
